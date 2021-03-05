2b85eb9817dcc4d118f7f6225e52afda10d4f2df
MJVenditti <67473940+MJVenditti@users.noreply.github.com>
2021-03-05 13:27:28 -0500

    Basic Layout Constructed

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$("#resume").mouseenter(function() {
+
+    $("#resume").css("text-decoration", "underline");
+
+});
+
+$("#resume").mouseleave(function() {
+
+    $("#resume").css("text-decoration", "none");
+
+});
+
+$("#linkedin").mouseenter(function() {
+
+    $("#linkedin").css("text-decoration", "underline");
+
+});
+
+$("#linkedin").mouseleave(function() {
+
+    $("#linkedin").css("text-decoration", "none");
+
+});
